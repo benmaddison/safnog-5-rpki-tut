272206c4fe599dcccdafa591d63246fbd9654d50
Ben Maddison <benm@workonline.africa>
2023-02-21 15:54:21 +0200

    work in progress

--- ccc/deploy.py
+++ ccc/deploy.py
@@ -17,7 +17,7 @@ PARAMS = {"username": "root", "password": "safnog"}
 def main():
     """Run dployment."""
     args = parse_args()
-    inventory = read_inventory()
+    inventory = read_inventory(args.policy)
     time.sleep(args.delay)
     with multiprocessing.Pool(len(inventory)) as pool:
         pool.map(deploy, inventory)
@@ -43,13 +43,14 @@ def parse_args():
     return args
 
 
-def read_inventory():
+def read_inventory(policy):
     """Read inventory data from file."""
     inventory_path = os.path.join(os.path.dirname(__file__), "inventory.yml")
     with open(inventory_path) as f:
         data = yaml.safe_load(f)
-    inventory = [{"my": i, "other": [j for j in data if j["name"] != i["name"]]}  # noqa
-                 for i in data]
+    inventory = [{"my": i,
+                  "other": [j for j in data if j["name"] != i["name"]],
+                  "policy": policy} for i in data]
     return inventory
 
 
@@ -57,11 +58,16 @@ def deploy(context):
     """Render config and deploy."""
     driver = napalm.get_network_driver(context["my"]["vendor"])
     candidate = render(context)
-    with driver(hostname=context["my"]["name"], **PARAMS) as device:
-        device.load_replace_candidate(config=candidate)
-        diff = device.compare_config()
-        print("Diff for {}:\n{}".format(context["my"]["name"], diff))
-        device.commit_config()
+    try:
+        with driver(hostname=context["my"]["name"], **PARAMS) as device:
+            device.load_replace_candidate(config=candidate)
+            diff = device.compare_config()
+            print("Diff for {}:\n{}".format(context["my"]["name"], diff))
+            device.commit_config()
+    except Exception as e:
+        print("Error while deploying config to {}:\n{}".format(context["my"]["name"],  # noqa
+                                                               candidate))
+        raise e
     print("Changes committed to {}".format(context["my"]["name"]))
     return
 
